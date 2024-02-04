3993b1c2830a6790280176eca933a69ede8f7c75
margaretlyon21 <156958807+margaretlyon21@users.noreply.github.com>
2024-02-04 12:49:52 -0500

    Update testfile.cpp

--- testfile.cpp
+++ testfile.cpp
@@ -6,56 +6,56 @@ using namespace std;
 
 int main(){
 while (true){
-     // Prompt user to input values for obj1
+     // input values for obj1
         int littles1, lots1, heaps1;
-        std::cout << "Enter values for obj1 (littles lots heaps): ";
-        std::cin >> littles1 >> lots1 >> heaps1;
+        cout << "Enter values for obj1 (littles lots heaps): ";
+        cin >> littles1 >> lots1 >> heaps1;
 
         Measure obj1(littles1, lots1, heaps1);
 
-        // Prompt user for the operation to perform
+        // choose operation
         char operation;
-        std::cout << "Enter the number for the operation to perform (1. +, 2. -, 3.  /, 4.  *, 5.  ==): ";
-        std::cin >> operation;
+        cout << "Enter the number for the operation to perform (1. +, 2. -, 3.  /, 4.  *, 5.  ==): ";
+        cin >> operation;
 
-        // Prompt user to input values for obj2
+        // input values for obj2
         int littles2, lots2, heaps2;
-        std::cout << "Enter values for obj2 (littles lots heaps): ";
-        std::cin >> littles2 >> lots2 >> heaps2;
+        cout << "Enter values for obj2 (littles lots heaps): ";
+        cin >> littles2 >> lots2 >> heaps2;
 
         Measure obj2(littles2, lots2, heaps2);
 
     //Branching to chosen operation
     if (operation == 1){
         // obj1 + obj2
-        Measure result_add = obj1 + obj2;
-        cout << "Addition: " << static_cast<string>(result_add) << endl;
+        Measure result = obj1 + obj2;
+        cout << "Result: " << static_cast<std::string>(result) << endl;
 
     }
     else if(operation ==2){
         // obj1 - obj2
-        Measure result_sub = obj1 - obj2;
-        cout << "Subtraction: " << static_cast<string>(result_sub) << endl;
+        Measure result = obj1 - obj2;
+        cout << "Result: " << static_cast<std::string>(result) << endl;
 
     }
     else if (operation == 3){
         //obj1 / obj2
-        Measure result_divide = obj1 / obj2;
-        cout << "Divide: " << static_cast<string>(result_divide) << endl;
+        Measure result = obj1 / obj2;
+        cout << "Result: " << static_cast<std::string>(result) << endl;
+
 
     }
-
     else if (operation ==4){
         // obj1 * obj2
-        Measure result_mult = obj1 * obj2;
-        cout << "Multiplication: " << static_cast<string>(result_mult) << endl;
+        Measure result = obj1 * obj2;
+        cout << "Result: " << static_cast<std::string>(result) << endl;
 
 
     }
     else if (operation ==5){
         //obj1 == obj2
-        bool result_equal = (obj1 == obj2);
-        if (result_equal) {
+        bool result = (obj1 == obj2);
+        if (result) {
 		    cout << "Equal: True" << endl;
 	    }
 	    else {
@@ -66,7 +66,6 @@ while (true){
     else{
         cout << "Invalid operation entered. Please try again.";
     }
-
     //allow user to perform actions again
     cout << "Do you wish to continue? (y/n)";
     char yn;
@@ -78,5 +77,4 @@ while (true){
 }
 
 
-}
 }
